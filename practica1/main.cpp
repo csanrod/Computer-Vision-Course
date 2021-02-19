90428c0e07d0d0b5b272eb319709bc61f675c54f
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2021-02-19 09:40:22 +0100

    Image loaded and showed successfully

--- practica1/main.cpp
+++ practica1/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace cv;
 using namespace std;
 
 int main( int argc, char** argv ) {
     cout << "INIT\n";
+    const string RELATIVE_PATH = "../../vision/images/RGB.jpg";
+
+    Mat src = imread( RELATIVE_PATH, IMREAD_COLOR );
+    if ( src.empty() ) {
+        cout << "Could not open or find the image!\n" << endl;
+        cout << "Usage: " << argv[0] << " <Input image>" << endl;
+        return -1;
+    }
+
+    // Show image
+    namedWindow( "Practise 1", WINDOW_AUTOSIZE );
+    imshow("Practise 1", src);
+
+    // Wait to press a key
+    waitKey(0);
+
     return 0;
 }
