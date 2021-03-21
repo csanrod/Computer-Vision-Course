8fd8a6306ece3013c7cafa261c93facdc4ddb7f8
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2021-03-21 12:55:46 +0100

    shrink with input to compare

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -202,8 +202,9 @@ void sliderCallback (int a, void * arg)
     const float* histRange = { range };
     bool uniform = true, accumulate = false;
 
-    Mat hist;
+    Mat hist, input_hist;
     calcHist( &histogram, 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &image_input, 1, 0, Mat(), input_hist, 1, &histSize, &histRange, uniform, accumulate );
     
 
     int hist_w = 512, hist_h = 400;
@@ -212,14 +213,19 @@ void sliderCallback (int a, void * arg)
     Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
 
     normalize(hist, hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(input_hist, input_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
 
     for( int i = 1; i < histSize; i++ ) {
         line(histImage, Point( bin_w*(i-1), hist_h - cvRound(hist.at<float>(i-1)) ),
              Point( bin_w*(i), hist_h - cvRound(hist.at<float>(i)) ),
              Scalar( 255, 0, 0), 2, 8, 0);
+
+        line(histImage, Point( bin_w*(i-1), hist_h - cvRound(input_hist.at<float>(i-1)) ),
+             Point( bin_w*(i), hist_h - cvRound(input_hist.at<float>(i)) ),
+             Scalar( 0, 0, 255), 2, 8, 0  );
     }
 
-    imshow("Mojón", histImage);
+    imshow("Shrink", histImage);
     imshow(WINDOW_NAME, histogram);
     //cout << "-- Me ejecuto crack! --" << endl;
 }
