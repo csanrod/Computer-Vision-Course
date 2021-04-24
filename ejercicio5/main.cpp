82a5b95028b6744216ea49ab141acabb9dbe13a8
csanrod <c.sanchezro.2018@alumnos.urjc.es>
2021-04-24 12:31:23 +0200

    Ejercicio5 v1.0.0

--- ejercicio5/main.cpp
+++ ejercicio5/main.cpp
@@ -3,13 +3,9 @@
 //
 // Asignatura: Visión Artificial (Ejercicio 5)
 //
-// Descripción: Este ejercicio consiste en aplicar diversos modos  
-//				para obtener contornos/bordes de la imagen damas.jpg 
-//
-//				Modos aplicados: 
-//				    - Hough
-//				    - Contours
-//				    - Centroids
+// Descripción: En este programa, se busca obtener el esqueleto de una  
+//				imagen  binarizada. Se mostrará el proceso durante el
+//              display.
 /* ------------------------------------------------------------ */
 
 // -- Includes -- //
@@ -17,7 +13,6 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
-#include <cmath>
 #include <iostream>
 #include <iomanip>
 
@@ -25,7 +20,7 @@ using namespace cv;
 using namespace std;
 
 // -- Constantes y variables globales -- //
-enum slider_mode 
+enum element_type
 {
     RECT = 0,
     CROSS,
@@ -33,17 +28,18 @@ enum slider_mode
 };
 
 const String WINDOW_NAME = "Skeleton Demo";
+int counter = 0;    // Contador de iteraciones
 
 // Matrices empleadas para procesar
 Mat image_input,
+    skeleton_mat,
+    open_mat,
     image_output;
 
-// Variables de los sliders
+// Variables de los sliders (inicializadas)
 int iterations = 60,
-    element = RECT,
+    element = CROSS,
     kernel_size = 1;
-
-RNG rng(12345);
 
 // Método de trazado para depurar sliders
 void print_debug_info ()
@@ -58,28 +54,74 @@ void print_debug_info ()
 // Método Callback
 void sliderCallback (int a, void * arg) 
 {
-    switch (element)
-    {
-        case RECT:
-        {
-            cout << "RECT selected" << endl;
-            break;
-        }
+    image_output = image_input.clone();
+    bool done = false;                // Condición del loop para saber si han finalizado las iteraciones    
+    Mat tresh = image_input.clone();
 
-        case CROSS:
-        {
-            cout << "CROSS selected" << endl;
-            break;
-        }
+    // Aplico un threshold para obtener resultados más consistentes.
+    // En función del valor del thresh, interpreta el esqueleto mejor o peor.
+    threshold( image_input, 
+               tresh, 
+               127, 
+               255,  
+               THRESH_BINARY);
 
-        case ELLIPSE:
-        {
-            cout << "ELLIPSE selected" << endl;
-            break;
+    Mat struct_element = getStructuringElement( element, 
+                                                Size( 2*kernel_size + 1, 2*kernel_size + 1 ), 
+                                                Point( kernel_size, kernel_size ) );
+
+    Mat eroded, temp;
+    Mat skel = Mat::zeros(tresh.size(), CV_8UC1);
+
+    while ((!done) && (iterations != 0)) {
+        // Open
+        morphologyEx( tresh, 
+                      eroded, 
+                      MORPH_ERODE, 
+                      struct_element );
+
+        morphologyEx( eroded, 
+                      temp, 
+                      MORPH_DILATE, 
+                      struct_element );
+
+        //Subtract
+        subtract(tresh, temp, temp);
+        // Union
+        bitwise_or(skel, temp, skel);
+        // Redefinir erosionada
+        eroded.copyTo(tresh);
+        
+        if (counter == iterations-1)
+            done = true;
+
+        counter++;
+        imshow(WINDOW_NAME, skel); 
+        waitKey(50);
+    };
+    waitKey(1000);
+    counter = 0;
+    
+    // Display, se pinta en rojo los píxeles de skel en la imagen original
+    cvtColor(image_output, image_output, COLOR_GRAY2BGR);
+    vector<Mat> out_channels, channels;
+    split( image_output, out_channels );
+
+    for (int row = 0; row < skel.rows; row++){
+        for (int col = 0; col < skel.cols; col++){
+            if ((uint)skel.at<uchar>(row, col) == 255){
+                    out_channels[0].at<uchar>(row, col) = 0;
+                    out_channels[1].at<uchar>(row, col) = 0;
+                    out_channels[2].at<uchar>(row, col) = 255;
+            }
         }
     }
+    
+    channels.push_back(out_channels[0]);
+    channels.push_back(out_channels[1]);
+    channels.push_back(out_channels[2]);
 
-    print_debug_info ();
+    merge(channels, image_output);
     imshow(WINDOW_NAME, image_output);
 }
 
@@ -87,15 +129,15 @@ void sliderCallback (int a, void * arg)
 int main(int argc, char ** argv) {
     const char* filename = argc >=2 ? argv[1] : "./model.png";
 
-    // Input en color de imagen damas.jpg
-    image_input = imread( samples::findFile( filename ), IMREAD_COLOR);
+    // Input
+    image_input = imread( samples::findFile( filename ), IMREAD_GRAYSCALE);
     if( image_input.empty()){
         cout << "Error opening image" << endl;
         return EXIT_FAILURE;
     }
 
     // Resize
-    resize(image_input, image_input, Size(512, 512));
+    resize(image_input, image_input, Size(512, 512));    
     image_output = image_input.clone();
 
     // Sliders
